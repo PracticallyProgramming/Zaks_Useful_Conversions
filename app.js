c3a676f6a5ba46feb0944618641ddaca980b384d
Zachary Dulac <zaktak@optonline.net>
2021-05-13 23:41:38 -0400

    fixed port binding error

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-app.listen(3000, () => console.log('listening at 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
